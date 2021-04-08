52b74decd3fe6871628a24a69b0af5f562bde568
Cindy3184 <cindykrampah@yahoo.com>
2021-04-07 21:32:21 -0400

    added state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+import React, { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 import GroceryList from './components/GroceryList'
 
 function App() {
+// [value, function that allows you to change value]
+  const [item, setItem] = useState('');
+  const [list, setList] = useState([]);
+
   return (
-    <div className="App">
+    <div className='App'>
       <header>
         <h1>Today's Grocery List</h1>
       </header>
-      <Form />
+      {/* passed setItem into the form.js AKA PROPS*/}
+      <Form 
+        item={item} 
+        list={list} 
+        setList={setList} 
+        setItem={setItem} 
+      /> 
+      
       <GroceryList />
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,30 @@
 import React from 'react';
 
-const Form = () => {
+const Form = ({ item, setItem, list, setList }) => {
+    const itemHandler = (e) => {
+        console.log(e.target.value);
+        setItem(e.target.value);
+    };
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        setList([
+            ...list, {text: item, completed: false, id: Math.random() * 1000}
+        ]);
+        setItem('');
+    };
+
     return(
         <form>
-            <input type="text" className="grocery-input" />
-            <button className="grocery-button" type="submit">
+            <input onChange={itemHandler} type='text' className='grocery-input' value={item} />
+            <button onClick={submitHandler} className='grocery-button' type='submit'>
                 {/* //add font icon here */}
             </button>
-            <div className="select">
-                <select name="groceries" className="filter">
-                    <option value="all">All</option>
-                    <option value="completed">Completed</option>
-                    <option value="incomplete">Incomplete</option>
+            <div className='select'>
+                <select name='groceries' className='filter'>
+                    <option value='all'>All</option>
+                    <option value='completed'>Completed</option>
+                    <option value='incomplete'>Incomplete</option>
                 </select>
             </div>
         </form>
