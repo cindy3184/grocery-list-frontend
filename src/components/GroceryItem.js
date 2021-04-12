36799d9050e736c123c923858fcedcef264e8855
Cindy3184 <cindykrampah@yahoo.com>
2021-04-12 12:46:07 -0400

    update css

--- src/components/GroceryItem.js
+++ src/components/GroceryItem.js
@@ -6,12 +6,15 @@ const GroceryItem = ({ text, list, lists, setList }) => {
         setList(lists.filter((element) => element.id !== list.id));
     };
     return(
+        <section className='grocery-section'>
         <div className='grocery-container'>
             <li className='grocery-item'>{text}
-            <button className="complete-btn"><i className='fas fa-check'></i></button>
             <button onClick={deleteHandler} className="delete-btn"><i className='fas fa-trash'></i></button>
+            <button className="complete-btn"><i className='fas fa-check'></i></button>
             </li>
         </div>
+
+        </section>
 
     );
 };
